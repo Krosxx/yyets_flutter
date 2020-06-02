41ffe9cca9e6f1480f4f53d03c83cec098565f80
Vove <1132412166@qq.com>
2020-06-02 20:13:29 +0800

    tag: 1.0.9

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  static final String VERSION = "1.0.8";
+  static final String VERSION = "1.0.9";
 
   @override
   State createState() => AppState();
